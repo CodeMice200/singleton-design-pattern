e73cffab96f91581ec04aea529661421ed4b2ffc
CodeMice200 <Codemice200@gmail.com>
2024-11-16 18:34:16 +0530

    Add files via upload

---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,8 @@
+﻿using SingletonDesignPattern;
+
+Singleton s1 = Singleton.GetInstance();
+Singleton s2 = Singleton.GetInstance();
+
+Console.WriteLine(object.ReferenceEquals(s1, s2));
+
+Console.ReadKey();
---
+++ SingletonDesignPattern/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern
+{
+    public sealed class Singleton
+    {
+        private static Singleton _instance;
+
+        private Singleton()
+        {
+            Console.WriteLine("An instance has been created");
+        }
+
+        public static Singleton GetInstance()
+        {
+            if(_instance == null )
+            {
+                _instance= new Singleton();
+            }
+            return _instance;
+        }
+    }
+}
